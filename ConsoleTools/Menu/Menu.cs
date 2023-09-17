d119153d2d65a84f0a2519bc056bc04bbd69e101
Neelov Eugene <tmarrelo@mail.ru>
2023-09-17 03:11:44 +0300

    Добавление новых положений меню (Недоработано).

--- ConsoleTools/Menu/Menu.cs
+++ ConsoleTools/Menu/Menu.cs
@@ -5,20 +5,80 @@ public class Menu
     private int SelectedIndex;
     private List<Option> Options;
     private string Prompt;
+    private MenuPosition Position;
 
-    public Menu(string promt, IEnumerable<Option> options)
+    public Menu(string promt, IEnumerable<Option> options, MenuPosition position = MenuPosition.Center)
     {
         Prompt = promt;
         Options = options.ToList();
         SelectedIndex = 0;
+        Position = position;
     }
 
-    private void DisplayOptions()
+    private (int x, int y) GetCoordinates(MenuPosition position)
+    {
+        var windowHeight = Console.WindowHeight;
+        var windowWidth = Console.WindowWidth;
+        var maxOptionNameLength = Options.Max(option => option.Name.Length);
+
+        var verticalPosition = 0;
+        var horizontalPosition = 0;
+
+        switch (position)
+        {
+            case MenuPosition.TopLeft:
+                verticalPosition = 0;
+                horizontalPosition = 0;
+                break;
+            case MenuPosition.TopCenter:
+                verticalPosition = 0;
+                horizontalPosition = (windowWidth - maxOptionNameLength) / 2;
+                break;
+            case MenuPosition.TopRight:
+                verticalPosition = 0;
+                horizontalPosition = windowWidth - maxOptionNameLength;
+                break;
+            case MenuPosition.CenterLeft:
+                verticalPosition = (windowHeight - Options.Count) / 2;
+                horizontalPosition = 0;
+                break;
+            case MenuPosition.Center:
+                verticalPosition = (windowHeight - Options.Count) / 2;
+                horizontalPosition = (windowWidth - maxOptionNameLength) / 2;
+                break;
+            case MenuPosition.CenterRight:
+                verticalPosition = (windowHeight - Options.Count) / 2;
+                horizontalPosition = windowWidth - maxOptionNameLength;
+                break;
+            case MenuPosition.BottomLeft:
+                verticalPosition = windowHeight - Options.Count;
+                horizontalPosition = 0;
+                break;
+            case MenuPosition.BottomCenter:
+                verticalPosition = windowHeight - Options.Count;
+                horizontalPosition = (windowWidth - maxOptionNameLength) / 2;
+                break;
+            case MenuPosition.BottomRight:
+                verticalPosition = windowHeight - Options.Count;
+                horizontalPosition = windowWidth - maxOptionNameLength;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(position), position, null);
+        }
+
+        return (horizontalPosition, verticalPosition);
+    }
+
+    private void DisplayOptions(MenuPosition position)
     {
         Console.Clear();
-        Console.WriteLine(Prompt);
-
         var maxOptionNameLength = Options.Max(option => option.Name.Length);
+
+        var coordinates = GetCoordinates(position);
+        var horizontalPosition = coordinates.x;
+        var verticalPosition = coordinates.y;
+
+        Console.SetCursorPosition(horizontalPosition, verticalPosition);
 
         for (var i = 0; i < Options.Count; i++)
         {
@@ -61,12 +121,12 @@ public class Menu
 
     private Option GetCurrentOption() => Options.Count == 0 ? null : Options[SelectedIndex];
 
-    public string Run()
+    public string Show()
     {
         ConsoleKey key;
         do
         {
-            DisplayOptions();
+            DisplayOptions(Position);
             key = Console.ReadKey(true).Key;
 
             switch (key)
@@ -91,12 +151,25 @@ public class Menu
     public void Close() => Console.Clear();
 }
 
+public enum MenuPosition
+{
+    TopLeft,
+    TopCenter,
+    TopRight,
+    CenterLeft,
+    Center,
+    CenterRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight
+}
+
 public class Option
 {
     public readonly string Name;
     public readonly string? Description;
 
-    internal Option(string name, string? description = null)
+    public Option(string name, string? description = null)
     {
         Name = name;
         Description = description;
