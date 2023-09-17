b4455286089911d5cc541785bfc577ff76ab6e91
Neelov Eugene <tmarrelo@mail.ru>
2023-09-18 01:12:36 +0300

    Небольшая доработка индикатора состояния.

--- ConsoleTools/ProgressBar/ProgressBar.cs
+++ ConsoleTools/ProgressBar/ProgressBar.cs
@@ -7,17 +7,25 @@ public class ProgressBar
     private int Total;
     private int Current;
     private int BarLength;
+    private char BarSymbol;
     private ConsoleColor BarColor;
+    private IEnumerable<char> BorderChar;
     private ConsoleColor BorderColor;
     private string Information;
 
-    public ProgressBar(int total, int barLength = 40, ConsoleColor barColor = ConsoleColor.Green,
+    public ProgressBar(int total = 100, 
+        int barLength = 40,
+        char barSymbol = '■',
+        ConsoleColor barColor = ConsoleColor.Green,
+        IEnumerable<char>? borderChar = null,
         ConsoleColor borderColor = ConsoleColor.Gray)
     {
         Total = total;
         Current = 0;
         BarLength = barLength;
+        BarSymbol = barSymbol;
         BarColor = barColor;
+        BorderChar = borderChar ?? new List<char> { '[', ']' };
         BorderColor = borderColor;
         Information = string.Empty;
     }
@@ -36,13 +44,14 @@ public class ProgressBar
         var progress = (double)Current / Total;
         var progressBarLength = (int)(progress * BarLength);
 
-        Write("[");
+        SetColors(BorderColor);
+        Write(BorderChar.ToList()[0]);
         for (var i = 0; i < BarLength; i++)
         {
             if (i < progressBarLength)
             {
                 SetColors(BarColor);
-                Write("■");
+                Write(BarSymbol);
             }
             else
             {
@@ -52,7 +61,7 @@ public class ProgressBar
         }
 
         SetColors(BorderColor);
-        Write($"] {Current}/{Total} {Information}");
+        Write($"{BorderChar.ToList()[1]} {Current}/{Total} {Information}");
         ResetColor();
     }
 
