4063d9d2ba959121011cc2978965d5d6eb667f7a
mamba <neelov.ev@mail.ru>
2023-10-13 16:03:01 +0300

    доработка прогресс бара

--- ConsoleTools/ProgressBar/ProgressBar.cs
+++ ConsoleTools/ProgressBar/ProgressBar.cs
@@ -1,10 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace ConsoleTools.ProgressBar;
 
 public class ProgressBar
 {
-    private int Total;
+    public int Total;
     private int Current;
     private int BarLength;
     private char BarSymbol;
@@ -12,8 +15,12 @@ public class ProgressBar
     private IEnumerable<char> BorderChar;
     private ConsoleColor BorderColor;
     private string Information;
+    private string Title;
+    private ProgressBarPosition TitlePosition;
+    private ProgressBarPosition BarPosition;
 
-    public ProgressBar(int total = 100, 
+    public ProgressBar(string title, ProgressBarPosition titlePosition, ProgressBarPosition barPosition,
+        int total = 100,
         int barLength = 40,
         char barSymbol = '■',
         ConsoleColor barColor = ConsoleColor.Green,
@@ -28,8 +35,11 @@ public class ProgressBar
         BorderChar = borderChar ?? new List<char> { '[', ']' };
         BorderColor = borderColor;
         Information = string.Empty;
+        Title = title;
+        TitlePosition = titlePosition;
+        BarPosition = barPosition;
     }
-    
+
     public void Next(int value)
     {
         if (value < 0 || value > Total)
@@ -38,14 +48,88 @@ public class ProgressBar
         Current = value;
     }
 
-    public void Draw()
+    private (int x, int y) GetTitlePosition(int width, int height)
     {
-        SetCursorPosition(0, CursorTop);
+        int x = 0;
+        int y = 0;
+
+        switch (TitlePosition)
+        {
+            case ProgressBarPosition.Above:
+                y = 0;
+                break;
+            case ProgressBarPosition.Below:
+                y = height - 1;
+                break;
+            case ProgressBarPosition.Center:
+                y = height / 2;
+                break;
+        }
+
+        return (x, y);
+    }
+
+    private (int x, int y) GetBarPosition(int width, int height)
+    {
+        var x = 0;
+        var y = 0;
+
+        switch (BarPosition)
+        {
+            case ProgressBarPosition.Above:
+                y = 1;
+                break;
+            case ProgressBarPosition.Below:
+                y = height - 1;
+                break;
+            case ProgressBarPosition.Center:
+                y = height / 2;
+                break;
+            case ProgressBarPosition.Left:
+                x = 0;
+                break;
+            case ProgressBarPosition.Right:
+                x = width - BarLength;
+                break;
+            case ProgressBarPosition.TopLeft:
+                x = 0;
+                y = 1;
+                break;
+            case ProgressBarPosition.TopRight:
+                x = width - BarLength;
+                y = 1;
+                break;
+            case ProgressBarPosition.BottomLeft:
+                x = 0;
+                y = height - 1;
+                break;
+            case ProgressBarPosition.BottomRight:
+                x = width - BarLength;
+                y = height - 1;
+                break;
+        }
+
+        return (x, y);
+    }
+
+    private void DrawTitle(int width, int height)
+    {
+        var (x, y) = GetTitlePosition(width, height);
+        SetCursorPosition(x, y);
+        SetColors(BorderColor);
+        Write(Title);
+    }
+
+    private void DrawProgressBar(int width, int height)
+    {
+        var (x, y) = GetBarPosition(width, height);
+        SetCursorPosition(x, y);
         var progress = (double)Current / Total;
         var progressBarLength = (int)(progress * BarLength);
 
         SetColors(BorderColor);
         Write(BorderChar.ToList()[0]);
+
         for (var i = 0; i < BarLength; i++)
         {
             if (i < progressBarLength)
@@ -65,7 +149,31 @@ public class ProgressBar
         ResetColor();
     }
 
+    public void Draw()
+    {
+        Clear();
+
+        var width = WindowWidth;
+        var height = WindowHeight;
+
+        DrawTitle(width, height);
+        DrawProgressBar(width, height);
+    }
+
     private static void SetColors(ConsoleColor color) => ForegroundColor = color;
-    
+
     public void SetInformation(string info) => Information = info;
+}
+
+public enum ProgressBarPosition
+{
+    Above,
+    Below,
+    Center,
+    Left,
+    Right,
+    TopLeft,
+    TopRight,
+    BottomLeft,
+    BottomRight
 }
